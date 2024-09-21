eba8e4c3f4b12b9f2d3ef955aed4c4bb85da5c03
Jibran Kalia <jibran.kalia@gmail.com>
2024-09-21 16:18:38 -0500

    initial commit

---
+++ src/main/kotlin/com/jibrankalia/qadhakeeper/QadhaKeeperApplication.kt
@@ -0,0 +1,11 @@
+package com.jibrankalia.qadhakeeper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QadhaKeeperApplication
+
+fun main(args: Array<String>) {
+	runApplication<QadhaKeeperApplication>(*args)
+}
---
+++ src/test/kotlin/com/jibrankalia/qadhakeeper/QadhaKeeperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jibrankalia.qadhakeeper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QadhaKeeperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
