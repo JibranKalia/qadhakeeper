f8efbd5103e51f5c7dafb40e5b7e54e705401916
Jibran Kalia <jibran.kalia@gmail.com>
2024-09-27 05:40:07 -0500

    added a ping controller

---
+++ src/main/kotlin/com/jibrankalia/qadhakeeper/ping/PingController.kt
@@ -0,0 +1,11 @@
+package com.jibrankalia.qadhakeeper.ping
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/ping")
+class PingController() {
+  @GetMapping fun ping(): String = "pong"
+}
