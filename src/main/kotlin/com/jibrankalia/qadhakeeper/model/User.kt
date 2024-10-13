ec2cc80db919c36d419d401782ba5d16ca570c95
Jibran Kalia <jibran.kalia@gmail.com>
2024-10-13 11:33:16 -0500

    chore: run formatter

--- src/main/kotlin/com/jibrankalia/qadhakeeper/model/User.kt
+++ src/main/kotlin/com/jibrankalia/qadhakeeper/model/User.kt
@@ -5,33 +5,17 @@ import java.time.LocalDateTime
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
-import org.springframework.security.core.GrantedAuthority
-import org.springframework.security.core.userdetails.UserDetails
 
 @Entity
 @Table(name = "users")
 @EntityListeners(AuditingEntityListener::class)
-data class User (
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  val id: Long? = null,
-
-  @Column(nullable = false, unique = true)
-  val username: String,
-
-  @Column(nullable = false)
-  val password: String,
-
-  @Column(nullable = false)
-  val enabled: Boolean,
-
-  @CreatedDate
-  @Column(nullable = false)
-  val createdAt : LocalDateTime? = null,
-
-  @LastModifiedDate
-  @Column(nullable = false)
-  val updatedAt : LocalDateTime? = null,
+data class User(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null,
+    @Column(nullable = false, unique = true) val username: String,
+    @Column(nullable = false) val password: String,
+    @Column(nullable = false) val enabled: Boolean,
+    @CreatedDate @Column(nullable = false) val createdAt: LocalDateTime? = null,
+    @LastModifiedDate @Column(nullable = false) val updatedAt: LocalDateTime? = null,
 )
 // : UserDetails {
 //   override fun getAuthorities(): Collection<GrantedAuthority> = emptyList()
